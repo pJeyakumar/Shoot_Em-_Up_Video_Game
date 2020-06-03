d15c04ae5fe16450df10e4d8e5a2fa4fd4d5b8b9
Piranavan Jeyakumar <57971751+pJeyakumar@users.noreply.github.com>
2020-06-03 18:18:44 -0400

    Music Error
    Commenting out music issues

--- GameOver.cs
+++ GameOver.cs
@@ -1,4 +1,4 @@
-﻿// Piranavan Jeyakumar
+// Piranavan Jeyakumar
 // 01.16.2018
 // Game Over
 // Will display itself once player loses
@@ -17,17 +17,17 @@ namespace A8_PiranavanJeyakumar_Culiminating
     public partial class frmGameOver : Form
     {
         // Variable Declaration
-        System.Media.SoundPlayer np = new System.Media.SoundPlayer(@"E:\A8_PiranavanJeyakumar_Culiminating\A8_PiranavanJeyakumar_Culiminating\Resources\Turnabout Jazz Soul - Track 8 - Godot - The Fragrance of Dark Coffee.wav");
+        //System.Media.SoundPlayer np = new System.Media.SoundPlayer(@"E:\A8_PiranavanJeyakumar_Culiminating\A8_PiranavanJeyakumar_Culiminating\Resources\Turnabout Jazz Soul - Track 8 - Godot - The Fragrance of Dark Coffee.wav");
 
         public frmGameOver()
         {
             InitializeComponent();
-            np.PlayLooping();
+            //np.PlayLooping();
         }
         // Will go to HighScore form and hide this one
         private void btnToHighscores_Click(object sender, EventArgs e)
         {
-            np.Stop();
+            //np.Stop();
             this.Hide();
             frmHighScores HighScores = new frmHighScores();
             HighScores.Show();
